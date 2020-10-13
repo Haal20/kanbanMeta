a88f0bc6b5f21465784c010c011f1a3860e7fcb5
Joel Storck <stjo20zs@student.ju.se>
2020-10-13 19:21:02 +0200

    importerade moveCards i script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-import {moveCards} from "./modules/moveCards.mjs";
+
+import { CreateView, CreateKanBan} from './Modules/view.mjs';
+//Use this function for adding Header and footer
+
+import {logIn} from './Modules/logIn.mjs'
+
+logIn();
+
+import {moveCards} from "./Modules/moveCards.mjs";
 
 moveCards();
+
