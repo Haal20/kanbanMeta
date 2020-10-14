370246119ae99d0cb11c148ca8967490a59cfadf
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-10-14 09:52:32 +0200

    kom inte med vid förra sparandet

--- modules/printLayout.mjs
+++ modules/printLayout.mjs
@@ -1,30 +1,15 @@
-export function printLayout() {
-let cardArray = ["list1","list3","list2","list4","list3","list2","list4","list2","list1","list2","list3","list1","list4","list2"];
+import { createCardElement } from './view.mjs';
 
-let todo = "<ul>";
-let doing = "<ul>";
-let test = "<ul>";
-let done = "<ul>";
-cardArray.forEach(function(card) {
-  if(card == "list1"){
-    //sortera till todo div
-    todo += `<li>Kort 1 här...</li>`;
-  }if (card == "list2") {
-    //sortera till doing div
-    doing += `<li>Kort 2 här...</li>`;
-  }if (card == "list3") {
-    //sortera till test div
-    test += `<li>Kort 3 här...</li>`;
-  }if (card == "list4") {
-    //sortera till done div
-    done += `<li>Kort 4 här...</li>`;
-  }else {
-    console.log("Error...");
-  }
-});
-document.getElementById("todo").innerHTML +=  todo +"</ul>";
-document.getElementById("doing").innerHTML +=  doing +"</ul>";
-document.getElementById("test").innerHTML +=  test +"</ul>";
-document.getElementById("done").innerHTML +=  done +"</ul>";
+export function printLayout() {
+  
+    let cardArray = localStorage.getItem('myCards');
+    if (cardArray){
+      cardArray = JSON.parse(cardArray)
+        cardArray.forEach(cardElement => {
+            createCardElement(cardElement.type, cardElement.text)
+        })
+    }
+
+console.log(cardArray);
 
 }
