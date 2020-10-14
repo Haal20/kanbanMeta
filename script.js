64ce7b5daef593d23884b7bf3b5aa8ac86502775
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-10-14 10:10:48 +0200

    städar lite

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-import { CreateView, CreateKanBan} from './Modules/view.mjs';
-//Use this function for adding Header and footer
-
 import {logIn} from './Modules/logIn.mjs'
 
-logIn();
-
-//Use function for creating boards
-//CreateKanBan();
+logIn(); 
