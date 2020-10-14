4afa2d8b26ee95c42c827e821c28f829cc464256
Joel Storck <stjo20zs@student.ju.se>
2020-10-14 17:04:41 +0200

    testar att ändra script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-
-import { CreateView, CreateKanBan} from './Modules/view.mjs';
-//Use this function for adding Header and footer
-
 import {logIn} from './Modules/logIn.mjs'
 
-logIn();
-
-import {moveCards} from "./Modules/moveCards.mjs";
-
-moveCards();
-
+logIn(); 
