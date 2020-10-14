fd5c837e51b2294cb5ff8a88d49868814ef98fe9
Joel Storck <stjo20zs@student.ju.se>
2020-10-14 18:15:09 +0200

    importerade i script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+import {moveCards} from './Modules/moveCards.mjs'
+moveCards();
+
 import {logIn} from './Modules/logIn.mjs'
 
-<<<<<<< HEAD
-logIn(); 
-=======
 
 logIn(); 
- master
->>>>>>> 0213eddfb8eca562ba6c5ed25977c755f207a7af
+
+
+
