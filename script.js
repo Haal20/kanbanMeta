1f5d11de41fd8b76f0643fdfca859438d0660134
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-10-14 09:50:10 +0200

    tagit bort sortera funktionen och skriver nu ut kort från LocalStorage

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// importera fler funktioner med , mellan varje funktion
-import { printLayout } from "./modules/printLayout.mjs";
-
 import { CreateView, CreateKanBan} from './Modules/view.mjs';
 //Use this function for adding Header and footer
 
@@ -8,11 +5,5 @@ import {logIn} from './Modules/logIn.mjs'
 
 logIn();
 
-<<<<<<< HEAD
 //Use function for creating boards
-CreateKanBan();
-
-//Sort function to sort cards
-printLayout();
-=======
->>>>>>> 05a57766cd81619a76e50d7c7a58632b88039689
+//CreateKanBan();
